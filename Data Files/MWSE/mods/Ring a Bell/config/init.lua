7d72a9b8183312a6f8e293be70a16789efe97ac5
C3pa <41503714+C3pa@users.noreply.github.com>
2026-05-01 23:20:19 +0200

    Update config

--- Data Files/MWSE/mods/Ring a Bell/config/init.lua
+++ Data Files/MWSE/mods/Ring a Bell/config/init.lua
@@ -2,8 +2,7 @@ local configFile = "Ring a Bell"
 
 ---@class RingABell.defaultConfigTable
 local default = {
-	---@type mwseLoggerLogLevel
-	logLevel = "INFO",
+	logLevel = mwse.logLevel.info,
 	semitones = 2,
 }
 
@@ -12,7 +11,7 @@ local default = {
 ---@field default RingABell.defaultConfigTable
 
 local config = mwse.loadConfig(configFile, default) --[[@as RingABell.configTable]]
-config.version = "1.0.0"
+config.version = "1.1.0"
 config.default = default
 
 return config
